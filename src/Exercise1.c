8728d4ad3f2f672cb6c844e298a69b0d6b212883
Nguyen Thanh Hien <ITITIU20142@student.hcmiu.edu.vn>
2021-11-22 17:54:00 +0700

    1,3,5

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -9,10 +9,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 void Ex1(int n){
 	//Your codes here
-	
+	int count = 1;
+	while (n>=10){
+		n = n/10;
+		count++;
+	}
+	int len = count ;
+	printf("%d ", len);
+	char * ones[] = {"zero", "one", "two","three", "four",
+					"five", "six", "seven", "eight", "nine"};
+	char * tens[] = {"", "ten", "eleven", "twelve", "thỉteen",
+					 "fourteen", "fifteen", "sixteen", "seventeen",
+					 "eighteen", "nineteen"};
+	char * tensmul[] = {"", "", "twenty", "thirty", "forty",
+						"fifty", "sixty", "seventy", "eighty",
+						"ninety"};
+
+	if (len == 0){
+		printf("no number input!!!");
+	}
+	if(len > 4){
+		printf("number input maximum length is 4-digits!!!");
+	}
+	if(len == 1){ 
+		printf("%s \n", ones[n]);
+	}
+	if(len == 2){
+		if(n == 10){printf("ten");}
+		else if (n<=19){
+			printf("%s ", tens[n-9]);
+		}
+		else if(n%10 == 0 && n>=20){
+			printf("%s ", tensmul[(n/10)-1]);
+		}
+		else if (n > 20 && n%10 != 0){
+			int u = n%10;
+			printf("%s %s ", tensmul[(n/10) -1], ones[u]);
+		}
+	}
+	if(len == 3){
+		int h = n%1000;
+		printf("%s hundred ", ones[h/100]);
+		int d = h%100;
+		if (d >= 10 && d <= 19){
+			printf("and %s",tens[(d)-9]);
+		}
+		if (d <= 9 && d != 0){
+			printf("and %s", ones[d]);
+		}
+		if (d >= 20){
+			printf("%s ", tensmul[(d)/10]);
+			printf("%s ", ones[d%10]);
+		}
+		if (d == 0) {printf(" ");}
+	}
+	if(len == 4){
+		int t = n/1000;
+		printf("%s thousand ", ones[t]);
+		int h = n%1000;
+		printf("%s hundred ", ones[h/100]);
+		int d = h%100;
+		if (d >= 10 && d <= 19){
+			printf("and %s",tens[(d)-9]);
+		}
+		if (d <= 9 && d != 0){
+			printf("and %s", ones[d]);
+		}
+		if (d >= 20){
+			printf("%s ", tensmul[(d)/10]);
+			printf("%s ", ones[d%10]);
+		}
+		if (d == 0) {printf(" ");}
+	}
 }
 
 int main(int argc, char *argv[]) {
--- src/Exercise3.c
+++ src/Exercise3.c
@@ -15,7 +15,32 @@ Ex:
 
 void Ex3(char *str){
 	//Your codes here
-	
+	int len = strlen(str);
+	int start_i = 0, end_i = 0;
+	int min_len=len, min_index=0, max_len=0, max_index=0;
+	while (end_i<=len){
+		if(end_i<len && str[end_i] != '\0')
+		end_i++;
+		else{
+			int cur_len = end_i-start_i;
+			if (cur_len<min_len){
+				min_len=cur_len;
+				min_index=start_i;
+			}
+			if (cur_len>max_len){
+				max_len=cur_len;
+				max_index=start_i;
+			}
+			end_i++;
+			start_i = end_i;
+		}
+	}
+	char minstr[min_len];
+	memcpy( minstr, &str[min_index], min_len);
+	char maxstr[max_len];
+	memcpy( maxstr, &str[max_index], max_len);
+	printf("Shortest word: %s\n", minstr);
+	printf("Longest word: %s", maxstr);
 }
 
 int main(int argc, char *argv[]) {
